47976a05a32a268946e9447fce795b92124bf1f5
linuxdropout <dylan.hanner@yahoo.co.uk>
2018-12-20 23:40:10 +0000

    skiing challenged

---
+++ skiing-in-singapore/findLongestRoute.js
@@ -0,0 +1,82 @@
+const fs = require('fs')
+
+const convertToSkiiMap = (filePath) => {
+    const mapFile = fs.readFileSync(`${__dirname}/${filePath}`, 'UTF-8')
+    const skiiMap = mapFile
+        .replace(/\r/g,'')
+        .split('\n')
+        .map(line => line.split(' ').map(Number))
+
+    return skiiMap
+}
+
+const findLongestRoute = (map) => {
+    const [w, h] = map.shift()
+    const traversed_nodes = {}
+
+    let longest = []
+    let greatest = 0
+    for (let x = 0; x < w; x++) {
+        for (let y = 0; y < h; y++) {
+            const n = map[x][y]
+            const { path, diff } = findLongestDsc(n, x, y, w, h, map, traversed_nodes)
+            if (path.length > longest.length) {
+                longest = path
+                greatest = diff
+                continue
+            }
+            if (path.length === longest.length && diff > greatest) {
+                longest = path
+                greatest = diff
+                continue
+            }
+        }
+    }
+
+    return longest
+}
+
+const findLongestDsc = (n, x, y, w, h, map, traversed_nodes, current_path=[], current_diff=0, ) => {
+    if (typeof n === 'object') return n
+
+    const adjacentNodes = getAdjacentNodes(x, y, w, h, map)
+
+    let longest = []
+    let greatest = 0
+    for (const [x2, y2, n2] of adjacentNodes) {
+        const { path, diff } = n2 < n
+            ? findLongestDsc(n2, x2, y2, w, h, map, traversed_nodes, [...current_path, n], current_diff + n - n2)
+            : { path: [...current_path, n], diff: current_diff }
+        
+        if (path.length > longest.length) {
+            longest = path
+            greatest = diff
+            continue
+        }
+        if (path.length === longest.length && diff > greatest) {
+            longest = path
+            greatest = diff
+            continue
+        }
+    }
+
+    return {
+        path: longest,
+        diff: greatest
+    }
+}
+
+const getAdjacentNodes = (x, y, w, h, map) => {
+    const nodes = []
+    if (x > 1) nodes.push([x-1, y, map[x-1][y]])
+    if (y > 1) nodes.push([x, y-1, map[x][y-1]])
+    if (x < w-1) nodes.push([x+1, y, map[x+1][y]])
+    if (y < h-1) nodes.push([x, y+1, map[x][y+1]])
+
+    return nodes
+}
+
+module.exports = {
+    convertToSkiiMap,
+    findLongestRoute,
+}
---
+++ skiing-in-singapore/test.js
@@ -0,0 +1,28 @@
+const tape = require('tape')
+const {
+    convertToSkiiMap,
+    findLongestRoute,
+} = require('./findLongestRoute')
+
+const testMap1 = [
+    [4, 4],
+    [4, 8, 7, 3],
+    [2, 5, 9, 3],
+    [6, 3, 2, 5],
+    [4, 4, 1, 6],
+]
+tape('findLongestRoute :: Finds longest route 9-5-3-2-1 for testMap1', t => {
+    const route = findLongestRoute(testMap1)
+    t.ok(Array.isArray(route), 'returns an array')
+    t.equal(route.length, 5, 'of length 5')
+    t.deepEqual(route, [9, 5, 3, 2, 1], 'that is correct')
+    t.end()
+})
+
+tape('findLongestRoute :: Finds longest route for challenge map', t => {
+    const map = convertToSkiiMap('map.txt')
+    const route = findLongestRoute(map)
+    console.log(route)
+    t.ok(route)
+    t.end()
+})
